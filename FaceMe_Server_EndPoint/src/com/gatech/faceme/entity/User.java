f619330254b22aa40c87136ef9dd33ee076e00d2
eric108 <zjiang42@gatech.edu>
2013-10-08 22:39:17 -0400

    add constructor

--- FaceMe_Server_EndPoint/src/com/gatech/faceme/entity/User.java
+++ FaceMe_Server_EndPoint/src/com/gatech/faceme/entity/User.java
@@ -6,36 +6,54 @@ public class User {
 	private String faceKey;
 	private String gender;
 	private String school;
+	
+	private User(String userID, String password, 
+			String faceKey, String gender,String school){
+		this.userID = userID;
+		this.password = password;
+		this.faceKey = faceKey;
+		this.gender = gender;
+		this.school = school;
+	}
+
 	public String getUserID() {
 		return userID;
 	}
+
 	public void setUserID(String userID) {
 		this.userID = userID;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public String getFaceKey() {
 		return faceKey;
 	}
+
 	public void setFaceKey(String faceKey) {
 		this.faceKey = faceKey;
 	}
+
 	public String getGender() {
 		return gender;
 	}
+
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
+
 	public String getSchool() {
 		return school;
 	}
+
 	public void setSchool(String school) {
 		this.school = school;
 	}
-	
-	
+
 }
