ed50e4806fbac8d4b074b48ee866d0a8a229d696
Ziyi Jiang <zjiang42@gatech.edu>
2013-11-03 01:55:18 -0500

    implement getnotifiedpair and allpair. and tried two parameters input

--- FaceMe_Server/src/com/gatech/faceme/endpoints/PairTableEndpoint.java
+++ FaceMe_Server/src/com/gatech/faceme/endpoints/PairTableEndpoint.java
@@ -7,10 +7,11 @@ import javax.inject.Named;
 import javax.jdo.PersistenceManager;
 import javax.jdo.Query;
 
-import com.gatech.faceme.entity.User;
+import com.gatech.faceme.entity.PairTableEntity;
 import com.gatech.faceme.mediastore.PMF;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
+import com.google.appengine.api.datastore.Key;
 
 @Api(name = "pairtableendpoint", description = 
 		"This entity represents a pair table.", version = "v1")
@@ -18,13 +19,13 @@ public class PairTableEndpoint {
 	
 	@ApiMethod(httpMethod = "GET", name = "pairtable.list", path = "pairtable/list")
 	@SuppressWarnings({ "cast", "unchecked" })
-	public List<PairTableEndpoint> listPairTable() {
+	public List<PairTableEntity> listPairTable() {
 		PersistenceManager mgr = PMF.get().getPersistenceManager();
-		List<PairTableEndpoint> result = new ArrayList<PairTableEndpoint>();
+		List<PairTableEntity> result = new ArrayList<PairTableEntity>();
 		try {
-			Query query = mgr.newQuery(User.class);
+			Query query = mgr.newQuery(PairTableEntity.class);
 			for (Object obj : (List<Object>) query.execute()) {
-				result.add(((PairTableEndpoint) obj));
+				result.add(((PairTableEntity) obj));
 			}
 		} finally {
 			mgr.close();
@@ -32,17 +33,71 @@ public class PairTableEndpoint {
 		return result;
 	}
 	
-	@ApiMethod(httpMethod = "GET", name = "pairtable.get", path = "pairtable/get/{id}")
+	@ApiMethod(httpMethod = "GET", name = "pairtablenotification.get", 
+			path = "pairtable/getallnotification/{userid}")
 	@SuppressWarnings({ "cast", "unchecked" })
-	public PairTableEndpoint getUserByID(@Named("id") String id) {
+	public ArrayList<PairTableEntity> getNotNotifiedById(@Named("userid") String userID) {
 		PersistenceManager mgr = getPersistenceManager();
-		PairTableEndpoint pairtable = null;
+		ArrayList<PairTableEntity> result = new ArrayList<PairTableEntity>();
 		try {
-			pairtable = mgr.getObjectById(PairTableEndpoint.class, id);
+//			Query query = mgr.newQuery("select from PairTableEntity " +
+//										"where activeUser ==  mainuserid"+
+//										"parameters String mainuserid "
+//										);
+			Query query = mgr.newQuery(PairTableEntity.class, 
+					"(activeUser ==userID) && (ifNotified==false)");
+		
+			for (Object obj : (List<Object>) query.execute()) {
+				result.add(((PairTableEntity) obj));
+			}
+			
 		} finally {
 			mgr.close();
 		}
-		return pairtable;
+		return result;
+	}
+	
+	@ApiMethod(httpMethod = "GET", name = "pairtable.get", 
+			path = "pairtable/getallpairtable/{userid}")
+	@SuppressWarnings({ "cast", "unchecked" })
+	public ArrayList<PairTableEntity> getallpairtableById(@Named("userid") String userID) {
+		PersistenceManager mgr = getPersistenceManager();
+		ArrayList<PairTableEntity> result = new ArrayList<PairTableEntity>();
+		try {
+
+			Query query = mgr.newQuery(PairTableEntity.class, 
+					"activeUser ==userID");
+			
+			for (Object obj : (List<Object>) query.execute()) {
+				result.add(((PairTableEntity) obj));
+			}
+			
+		} finally {
+			mgr.close();
+		}
+		return result;
+	}
+	
+	@ApiMethod(httpMethod = "GET", name = "twoparameters.get", 
+			path = "pairtable/gettwoparameters/{userid}/{username}")
+	@SuppressWarnings({ "cast", "unchecked" })
+	public ArrayList<PairTableEntity> tryTwoParameters(
+			@Named("userid") String userID, @Named("username") String userName) {
+		PersistenceManager mgr = getPersistenceManager();
+		ArrayList<PairTableEntity> result = new ArrayList<PairTableEntity>();
+		try {
+
+			Query query = mgr.newQuery(PairTableEntity.class, 
+					"(activeUser ==userID) && (key ==userName)");
+			
+			for (Object obj : (List<Object>) query.execute()) {
+				result.add(((PairTableEntity) obj));
+			}
+			
+		} finally {
+			mgr.close();
+		}
+		return result;
 	}
 	
 	private static PersistenceManager getPersistenceManager() {
--- FaceMe_Server/src/com/gatech/faceme/entity/PairTableEntity.java
+++ FaceMe_Server/src/com/gatech/faceme/entity/PairTableEntity.java
@@ -16,11 +16,29 @@ public class PairTableEntity {
 	private Key key;
 	
 	@Persistent
+	private Key activeUser;
+	
+	@Persistent
 	private ArrayList<Key> faceList;
 	
-	public PairTableEntity(ArrayList<Key> faceList){
-		
+	@Persistent
+	private Key activeUserFace;
+	
+	@Persistent
+	private ArrayList<Key> otherUserFace;
+	
+	
+	@Persistent
+	private boolean ifNotified;
+	
+	public PairTableEntity(Key activeUser, ArrayList<Key> faceList,
+				Key activeUserFace, ArrayList<Key> otherUserFace,
+				boolean ifNotified){
+		this.activeUser = activeUser;
 		this.setFaceList(faceList);
+		this.setActiveUserFace(activeUserFace);
+		this.setOtherUserFace(otherUserFace);
+		this.ifNotified = ifNotified;
 	}
 
 	public Key getKey() {
@@ -38,6 +56,36 @@ public class PairTableEntity {
 	public void setFaceList(ArrayList<Key> faceList) {
 		this.faceList = faceList;
 	}
-	
+
+	public Key getActiveUser() {
+		return activeUser;
+	}
+
+	public void setActiveUser(Key activeUser) {
+		this.activeUser = activeUser;
+	}
+	public boolean getIfNotified() {
+		return ifNotified;
+	}
+
+	public void setIfNotified(boolean ifNotified) {
+		this.ifNotified = ifNotified;
+	}
+
+	public Key getActiveUserFace() {
+		return activeUserFace;
+	}
+
+	public void setActiveUserFace(Key activeUserFace) {
+		this.activeUserFace = activeUserFace;
+	}
+
+	public ArrayList<Key> getOtherUserFace() {
+		return otherUserFace;
+	}
+
+	public void setOtherUserFace(ArrayList<Key> otherUserFace) {
+		this.otherUserFace = otherUserFace;
+	}
 	
 }
